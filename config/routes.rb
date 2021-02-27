e1f4b4026c76aa987dead0154131626f55eecff0
EC2 Default User <ec2-user@ip-172-31-0-11.ec2.internal>
2021-02-27 01:53:24 +0000

    set_up_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  
+  root to: 'homes#index'
+  get 'homes/about'
+  devise_for :admin, controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+  namespace :admin do
+   get 'top/top'
+    
+  end
+  
+  
+  
+  devise_for :end_users, controllers: {
+    sessions:      'end_users/sessions',
+    passwords:     'end_users/passwords',
+    registrations: 'end_users/registrations'
+  }
+  scope module: :public do
+    get "/end_users", to: 'end_users#show'
+    get '/end_users/edit',to: 'end_users#edit'
+    patch '/end_users',to: 'end_users#update'
+    get 'end_users/out'
+    patch 'end_users/quit'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
